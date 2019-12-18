e9f372329a29ca35e49c6ee653d61e6c39a9bb58
时永宾 <shiyongbin@ezbuy.com>
2019-12-18 16:37:13 +0800

    header

--- interceptor_grpc_client.go
+++ interceptor_grpc_client.go
@@ -11,14 +11,15 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-func UnaryClientHeaderInterceptor(header http.Header) func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+func UnaryClientHeaderInterceptor() func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		md, ok := metadata.FromOutgoingContext(ctx)
 		if !ok {
 			return errors.New("metadata.FromOutgoingContext failed!")
 		}
 		md["http-client"] = []string{"interceptor/0.1"}
-		log.Debugf("%+v", md)
+		ctx = metadata.NewOutgoingContext(ctx, md)
+		log.Infof("%+v", md)
 		return invoker(ctx, method, req, reply, cc, opts...)
 	}
 }
